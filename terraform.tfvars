fbb50228a745abfacc601fcf7686b1af57de7c98
JendyJasper <jendy36@gmail.com>
2023-08-15 12:46:29 +0000

    completed aws networking

--- terraform.tfvars
+++ terraform.tfvars
@@ -7,3 +7,10 @@ enable_dns_support = "true"
 enable_dns_hostnames = "true"   
 
 preferred_number_of_public_subnets = 2
+
+tags = {
+  Enviroment      = "production" 
+  Owner-Email     = "jendydevops@gmail.com"
+  Managed-By      = "Terraform"
+  Billing-Account = "1234567890"
+}
--- variables.tf
+++ variables.tf
@@ -17,3 +17,18 @@ variable "enable_dns_hostnames" {
   variable "preferred_number_of_public_subnets" {
       default = 2
 }
+
+  variable "preferred_number_of_private_subnets" {
+      default = 4
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to all resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "name" {
+  description = "vpc name."
+  default     = "aws vpc"
+}
